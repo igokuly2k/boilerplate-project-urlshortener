35cb3e3671184f813e290d5721fe6b09e2e0308d
Gokul E <igokulsmailbox@gmail.com>
2022-01-27 00:13:25 +0530

    Project Working

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
+const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,7 +20,23 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
-
+let arr=[];
+app.get('/api/shorturl/:i',(req,res)=>{
+res.redirect(arr[req.params.i-1]);
+});
+app.post('/api/shorturl',(req,res)=>{
+  let url = req.body.url;
+  dns.lookup(url.replace(/(^\w+:\/\/)|(\/.*$)/g,""),(err,address,family)=>{
+    if(err) {
+      console.log(err);
+      res.json({error:'invalid url'});
+  }
+  else{
+    console.log(url);
+    arr.push(url);
+    res.json({original_url:url,short_url:arr.length});
+  }});
+});
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
